4acf5a3cd0508eb1e1d1700ce9fd6816bf9e8c8e
chris-avens <chrisianavendano@gmail.com>
2023-06-03 15:09:38 +0800

    unit conversion table completed, main code clean

--- main.py
+++ main.py
@@ -2,14 +2,45 @@ import pandas as pd
 
 # Read data from Excel file
 data = pd.read_excel("Perry Tables.xlsx")
+# relative path is not recognized if using scratch in python
 
 
 def unit_convert(initial_unit, desired_unit, magnitude):
     # Find row in data table corresponding to initial unit and desired unit
+    temp_units = ['Kelvin', 'Rankine', 'Celsius', 'Fahrenheit']
+
     row = data[(data['Customary or commonly used unit'] == initial_unit) &
                ((data['SI unit'] == desired_unit) | (data['Alternate SI unit'] == desired_unit))]
 
-    if row.empty:
+    if initial_unit in temp_units or desired_unit in temp_units:
+        match initial_unit + ' to ' + desired_unit:
+            case 'Kelvin to Rankine':
+                result = magnitude * 1.8
+            case 'Kelvin to Celsius':
+                result = magnitude - 273.15
+            case 'Kelvin to Fahrenheit':
+                result = magnitude * 1.8 - 459.67
+            case 'Celsius to Rankine':
+                result = magnitude * 1.8 + 32 + 459.67
+            case 'Celsius to Kelvin':
+                result = magnitude + 273.15
+            case 'Celsius to Fahrenheit':
+                result = magnitude * 1.8 + 32
+            case 'Rankine to Kelvin':
+                result = magnitude / 1.8
+            case 'Rankine to Celsius':
+                result = (magnitude - 32 - 459.67) / 1.8
+            case 'Rankine to Fahrenheit':
+                result = magnitude - 459.67
+            case 'Fahrenheit to Rankine':
+                result = magnitude + 459.67
+            case 'Fahrenheit to Kelvin':
+                result = (magnitude + 459.67) / 1.8
+            case 'Fahrenheit to Celsius':
+                result = (magnitude - 32) / 1.8
+            case True:
+                print("Could not find conversion factor for given units.")
+    elif row.empty:
         row = data[((data['SI unit'] == initial_unit) | (data['Alternate SI unit'] == initial_unit)) &
                    (data['Customary or commonly used unit'] == desired_unit)]
 
@@ -19,15 +50,10 @@ def unit_convert(initial_unit, desired_unit, magnitude):
             # Extract conversion factor from row
             conversion_factor = row.iloc[0]['Conversion factor; multiply customary unit by factor to obtain SI unit']
             result = magnitude / conversion_factor
-            print(f"{magnitude} {initial_unit} = {round(result,10)} {desired_unit}")
     else:
         # Extract conversion factor from row
         conversion_factor = row.iloc[0]['Conversion factor; multiply customary unit by factor to obtain SI unit']
         result = magnitude * conversion_factor
-        print(f"{magnitude} {initial_unit} = {round(result,10)} {desired_unit}")
 
+    return round(result, 10)
 
-# Get user input
-unit_convert(input("Enter initial unit: "),
-             input("Enter desired unit: "),
-             float(input("Enter magnitude: ")))
